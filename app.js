88d0aebada1aecbdd66e32c5425f30a06ba33225
Katriin Poolakese <katriin.poolaese@khk.ee>
2021-09-24 13:34:40 +0300

    ES

--- app.js
+++ app.js
@@ -3,43 +3,22 @@
 const firstName = 'Kadi';
 const surName = 'Tamm';
 const age = 20;
-const text = 'Tere, olen Kadi!';
 const tags = 'arendus,veeb';
 
 let val;
 
-// concatenation - text + text
-val = firstName + ' ' + surName;
+// concatenation - es5
+val = '<ul>' + + '</ul>'
 
-// append
-val = firstName;
-val += surName;
 
-val = text + ' Minu töö valdkond on ' + tags;
-
-// escaping \*
-val = 'See on \'jutumärkides\'';
-
-// case
-val = firstName.toLowerCase();
-val = surName.toUpperCase();
-
-// string index
-val = surName[0];
-val = surName.charAt(2); // viisakam vorm
-val = surName.charAt(-1); // ei saa
-val = surName.length;
-val = surName.charAt(surName.length - 1);
-
-val = surName.indexOf('a');
-val = surName.lastIndexOf('m'); // viimati ilmuv
-
-// string cutting
-val = surName.substring(0, 2);
-val = surName.slice(0, 2);
-val = surName.slice(-3);
-
-val = tags.split(',');
-
-// log
+// string templating - es6
+val = `
+	<ul>
+		<li>Eesnimi: ${firstName}</li>
+		<li>Perenimi: ${surName}</li>
+		<li>Vanus: ${age}</li>
+		<li>Alad: ${tags}</li>
+	</ul>
+`
 console.log(val);
+document.body.innerHTML = val;
