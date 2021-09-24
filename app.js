f1429378e8ff54815311fca263382327c4b14c16
Katriin Poolakese <katriin.poolaese@khk.ee>
2021-09-24 13:21:40 +0300

    Math operations

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-/* --- matemaatilised tehted --- */
+/* --- sõnetöötlus --- */
 
-const num1 = 100;
-const num2 = 50;
+const firstName = 'Kadi';
+const surName = 'Tamm';
+const age = 20;
+const text = 'Tere, olen Kadi!';
+const tags = 'arendus,veeb';
+
 let val;
 
-val = num1 + num2;
-val = num1 - num2;
-val = num1 * num2;
-val = num1 / num2;
-val = num1 % num2; // jääk
+// concatenation - text + text
+val = firstName + ' ' + surName;
 
-// math
-val = Math.PI;
-val = Math.round(2.5);
-val = Math.ceil(2.3); // ümardab üle
-val = Math.floor(2.8); // ümardab alla
-val = Math.sqrt(64);
-val = Math.abs(-3); // absoluutväärtus
-val = Math.pow(4, 2); // astendamine
+// append
+val = firstName;
+val += surName;
 
-val = Math.round(Math.random() * 10);
+val = text + ' Minu töö valdkond on ' + tags;
+
+// escaping \*
+val = 'See on \'jutumärkides\'';
+
+// case
+val = firstName.toLowerCase();
+val = surName.toUpperCase();
+
+// string index
+val = surName[0];
+val = surName.charAt(2); // viisakam vorm
+val = surName.charAt(-1); // ei saa
+val = surName.length;
+val = surName.charAt(surName.length - 1);
+
+val = surName.indexOf('a');
+val = surName.lastIndexOf('m'); // viimati ilmuv
+
+// string cutting
+val = surName.substring(0, 2);
+val = surName.slice(0, 2);
+val = surName.slice(-3);
+
+val = tags.split(',');
 
 // log
 console.log(val);
