3b954ba1da51f8f2fd24a19499239834bbb10525
Katriin Poolakese <katriin.poolaese@khk.ee>
2021-09-24 11:17:42 +0300

    Variables

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("test");
+// muutuja, mille sisu saab muuta
+let name = 'Kadi Tamm';
+name = 'Kadi Lepp';
+
+let firstName = 'Kadi';
+let $ = 'dollar';
+
+// muutuja, mis on konstantne
+const surName = 'Tamm';
+//surName = 'Lepp'
+
+// massiivid
+const numbers = [1, 2, 3, 4];
+numbers.push(6);
+
+// objektid
+const person = {
+	name: 'Kadi',
+	age: '20'
+};
+
+person.age = '21';
+
+// konsooli log
+console.log(numbers);
+console.log(person);
