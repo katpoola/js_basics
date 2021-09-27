3159efe5e88a8dc4b1998446af87228d8fba6555
Katriin Poolakese <katriin.poolaese@khk.ee>
2021-09-27 12:42:14 +0300

    ES5 ja 6

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ const tags = 'arendus,veeb';
 let val;
 
 // concatenation - es5
-val = '<ul>' + + '</ul>'
+val = '<ul>' + '<li>' + firstname + '</li>' + '<li>' + surname + '</li>' + '<li>' + age + '</li>' + '<li>' + tags + '</li>'+ '</ul>';
 
+val = '<ul>' + 
+		'<li>' + firstname + '</li>' + 
+		'<li>' + surname + '</li>' + 
+		'<li>' + age + '</li>' + 
+		'<li>' + tags + '</li>'+ 
+	'</ul>';		// vana versioon
 
 // string templating - es6
 val = `
@@ -19,6 +25,8 @@ val = `
 		<li>Vanus: ${age}</li>
 		<li>Alad: ${tags}</li>
 	</ul>
-`
+`		// parem versioon
+
+
 console.log(val);
 document.body.innerHTML = val;
