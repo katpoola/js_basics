a192b18ec6af1e20ba1a482f8a07a1433c9387b3
Katriin Poolakese <katriin.poolaese@khk.ee>
2021-09-27 14:19:22 +0300

    Date

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-/* --- objektideitöötlus --- */
+/* --- ajatöötlus --- */
 
-const person = {
-	firstName: 'Kadi',
-	lastName: 'Tamm',
-	age: 23,
-	email: 'kadi.tamm@gmail.com',
-	hobbies: ['music', 'sport'],
-	address: {
-		city: 'Tallinn',
-		county: 'Harjumaa'
-	},
-	getBirthdayYear: function(){
-		return 2021 - this.age;
-	}
-};
+const today = new Date();
 
-let val;
-val = person.firstName;
-val = person['lastName'];
-val = person.age;
-val = person.hobbies[1];
-val = person.address.city;
-val = person.address['city'];
-val = person.getBirthdayYear();
+val = today.getMonth(); // jaanuar = 0
+val = today.getDate();
+val = today.getDay();
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getSeconds();
+val = today.getMilliseconds();
+val = today.getTime();
 
-console.log(val);
+birthday = new Date('10/02/2021 12:00:36');
+
+birthday.setMonth(2);
+birthday.setDate(12);
+birthday.setFullYear(2020);
+birthday.setHours(17);
+birthday.setMinutes(38);
+birthday.setSeconds(40);
+birthday.setMilliseconds(15);
+
+console.log(today);
+console.log(birthday);
