bc6fca14b96be88dce4c2ad3823cf8809fd250ac
Katriin Poolakese <katriin.poolaese@khk.ee>
2021-09-27 15:11:22 +0300

    Switch and If

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
-let color = 'blue';
+let date = new Date();
+let weekDay = date.getDay();
 
-switch(color) {
-	case 'red':
-		console.log('STOP!');
-		break;
-	case 'yellow':
-		console.log('Attention!');
-		break;
-	case 'green':
-		console.log('GO!');
-		break;
-	default:
-		console.log('COLOR NOT ALLOWED!!!');
-		break;
+if (weekDay === 1) {
+	console.log('Esmaspäev');
+} else if (weekDay === 2) {
+	console.log('Teisipäev');
+} else if (weekDay === 3) {
+	console.log('Kolmapäev');
+} else if (weekDay === 4) {
+	console.log('Neljapäev');
+} else if (weekDay === 5) {
+	console.log('Reede');
+} else if (weekDay === 6) {
+	console.log('Laupäev');
+} else if (weekDay === 0) {
+	console.log('Pühapäev');
+} else {
+	console.log('ERROR!!!!!!!!');
 }
+
+
+switch(weekDay){
+	case 1:
+		console.log('Esmaspäev');
+		break;
+	case 2:
+		console.log('Teisipäev');
+		break;
+	case 3:
+		console.log('Kolmapäev');
+		break;
+	case 4:
+		console.log('Neljapäev');
+		break;
+	case 5:
+		console.log('Reede');
+		break;
+	case 6:
+		console.log('Laupäev');
+		break;
+	case 0:
+		console.log('Pühapäev');
+		break;
+};
