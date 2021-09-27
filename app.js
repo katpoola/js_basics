6e2dc50ef8b4a86982164142b7cf1a6f042d6de1
Katriin Poolakese <katriin.poolaese@khk.ee>
2021-09-27 13:27:05 +0300

    ES5 ja 6

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
-/* --- sõnetöötlus --- */
+/* --- massiivitöötlus --- */
 
-const firstName = 'Kadi';
-const surName = 'Tamm';
-const age = 20;
-const tags = 'arendus,veeb';
+const numbers = [43, 56, 33, 23, 44, 36, 5];
+const numbers2 = new Array(22, 45, 33, 76, 54);
+const fruits = ['õun', 'banaan', 'apelsin', 'virsik'];
+
+const mix = [22, 'Tere', true, undefined, null, {a:1 ,b:2}, new Date()];
 
 let val;
 
-// concatenation - es5
-val = '<ul>' + '<li>' + firstname + '</li>' + '<li>' + surname + '</li>' + '<li>' + age + '</li>' + '<li>' + tags + '</li>'+ '</ul>';
 
-val = '<ul>' + 
-		'<li>' + firstname + '</li>' + 
-		'<li>' + surname + '</li>' + 
-		'<li>' + age + '</li>' + 
-		'<li>' + tags + '</li>'+ 
-	'</ul>';		// vana versioon
+val = numbers.length; // massiivi pikkus
+val = Array.isArray(numbers); // massiivi kontroll
+val = numbers[3]
 
-// string templating - es6
-val = `
-	<ul>
-		<li>Eesnimi: ${firstName}</li>
-		<li>Perenimi: ${surName}</li>
-		<li>Vanus: ${age}</li>
-		<li>Alad: ${tags}</li>
-	</ul>
-`		// parem versioon
+numbers[2] = 100 // asendamine
 
+// lisamine
+numbers.push(250); // taha
+numbers.unshift(120); // ette
 
+// maha võtmine
+numbers.pop(); // tagant
+numbers.shift(); // eest
+
+// index
+val = numbers.indexOf(100);
+
+// lõikamine
+numbers.splice(2, 1);
+
+// ühendamine
+val = numbers.concat(numbers2);
+
+// sorteerimine
+val = fruits.sort();
+val = numbers.sort();  // ei tööta väga hästi
+
+val = numbers.sort(function(x, y){
+	return x - y;
+	}); // väiksemast suuremaks
+val = numbers.sort(function(x, y){
+	return y - x;
+	}); // suuremast väiksemaks
+
+// massiivi keskele lisamine
+val = numbers.splice(3, 0, 100);
+
+console.log(numbers);
 console.log(val);
-document.body.innerHTML = val;
