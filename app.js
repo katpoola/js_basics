5e6f0098d5fd1e107325a9a80e935d44b14146c1
Katriin Poolakese <katriin.poolaese@khk.ee>
2021-09-27 13:44:08 +0300

    Objects

--- app.js
+++ app.js
@@ -1,50 +1,27 @@
-/* --- massiivitöötlus --- */
+/* --- objektideitöötlus --- */
 
-const numbers = [43, 56, 33, 23, 44, 36, 5];
-const numbers2 = new Array(22, 45, 33, 76, 54);
-const fruits = ['õun', 'banaan', 'apelsin', 'virsik'];
-
-const mix = [22, 'Tere', true, undefined, null, {a:1 ,b:2}, new Date()];
+const person = {
+	firstName: 'Kadi',
+	lastName: 'Tamm',
+	age: 23,
+	email: 'kadi.tamm@gmail.com',
+	hobbies: ['music', 'sport'],
+	address: {
+		city: 'Tallinn',
+		county: 'Harjumaa'
+	},
+	getBirthdayYear: function(){
+		return 2021 - this.age;
+	}
+};
 
 let val;
+val = person.firstName;
+val = person['lastName'];
+val = person.age;
+val = person.hobbies[1];
+val = person.address.city;
+val = person.address['city'];
+val = person.getBirthdayYear();
 
-
-val = numbers.length; // massiivi pikkus
-val = Array.isArray(numbers); // massiivi kontroll
-val = numbers[3]
-
-numbers[2] = 100 // asendamine
-
-// lisamine
-numbers.push(250); // taha
-numbers.unshift(120); // ette
-
-// maha võtmine
-numbers.pop(); // tagant
-numbers.shift(); // eest
-
-// index
-val = numbers.indexOf(100);
-
-// lõikamine
-numbers.splice(2, 1);
-
-// ühendamine
-val = numbers.concat(numbers2);
-
-// sorteerimine
-val = fruits.sort();
-val = numbers.sort();  // ei tööta väga hästi
-
-val = numbers.sort(function(x, y){
-	return x - y;
-	}); // väiksemast suuremaks
-val = numbers.sort(function(x, y){
-	return y - x;
-	}); // suuremast väiksemaks
-
-// massiivi keskele lisamine
-val = numbers.splice(3, 0, 100);
-
-console.log(numbers);
 console.log(val);
