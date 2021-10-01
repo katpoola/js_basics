5e84ff075cea638a3ddb7c2cb0528f2ffb312b2e
Katriin Poolakese <katriin.poolaese@khk.ee>
2021-10-01 13:37:59 +0300

    DOM zebra

--- app.js
+++ app.js
@@ -2,16 +2,17 @@
 
 let val;
 
-val = document.getElementById('task-title');
-document.getElementById('task-title').style.background = '#000';
-document.getElementById('task-title').style.color = '#fff';
-document.getElementById('task-title').style.padding = '5px';
-document.getElementById('task-title').textContent = 'My Tasks';
-document.getElementById('task-title').innerText = 'My Favourite Tasks'
-document.getElementById('task-title').innerHTML = '<span style="color: red">My Tasks</span>'
-val = document.querySelector('h4');
-val = document.querySelector('#task-title');
-val = document.querySelector('.collection-item:nth-child(even)');
-val = document.querySelector('.collection-item:nth-child(odd)');
+val = document.querySelectorAll('.collection-item');
+val = document.querySelectorAll('li');
 
+oddLi = document.querySelectorAll('li:nth-child(odd)');
+evenLi = document.querySelectorAll('li:nth-child(even)');
+
+oddLi.forEach(function(li) {
+	li.style.background = '#aaa';	
+});
+evenLi.forEach(function(li) {
+	li.style.background = '#ccc'
+
+});
 console.log(val);
