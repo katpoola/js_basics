21b1d7b2026953b9404ff7127d6a080dc70baf6c
Katriin Poolakese <katriin.poolaese@khk.ee>
2021-10-04 13:20:23 +0300

    Keyboard events

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-/* ---  --- */
-// event elements
+/* --- event elements --- */
+
 const form = document.querySelector('form');
 const taskInput = document.querySelector('#task');
 
-form.addEventListener('submit', runEvent);
+// keyboard events
+
+// taskInput.addEventListener('keydown', runEvent);
+// taskInput.addEventListener('keyup', runEvent);
+// taskInput.addEventListener('keypress', runEvent);
+
+// focus & blur events
+
+// taskInput.addEventListener('focus', runEvent);
+// taskInput.addEventListener('blur', runEvent);
+
+// cut & paste events
+
+//taskInput.addEventListener('cut', runEvent);
+//taskInput.addEventListener('paste', runEvent);
+
+// input event
+taskInput.addEventListener('input', runEvent);
+
+
 
 function runEvent(e) {
 	console.log(`Event is ${e.type}`);
-	console.log(taskInput.value);
-	taskInput.value = '';
-	e.preventDefault(); // alati viimasena
+	console.log(e.target.value);
 };
 
