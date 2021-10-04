2d73f62e135367e97136e20ba769c2458833b331
Katriin Poolakese <katriin.poolaese@khk.ee>
2021-10-04 13:27:17 +0300

    Click events

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
 /* --- event elements --- */
 
-const form = document.querySelector('form');
-const taskInput = document.querySelector('#task');
+const list = document.querySelector('ul');
 
-// keyboard events
-
-// taskInput.addEventListener('keydown', runEvent);
-// taskInput.addEventListener('keyup', runEvent);
-// taskInput.addEventListener('keypress', runEvent);
-
-// focus & blur events
-
-// taskInput.addEventListener('focus', runEvent);
-// taskInput.addEventListener('blur', runEvent);
-
-// cut & paste events
-
-//taskInput.addEventListener('cut', runEvent);
-//taskInput.addEventListener('paste', runEvent);
-
-// input event
-taskInput.addEventListener('input', runEvent);
-
+list.addEventListener('click', runEvent);
 
 
 function runEvent(e) {
 	console.log(`Event is ${e.type}`);
 	console.log(e.target.value);
 };
-
