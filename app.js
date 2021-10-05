0fbcf6520d7993c8a2d7cc431f136c6a32af9154
Katriin Poolakese <katriin.poolaese@khk.ee>
2021-10-05 15:35:03 +0300

    Deleting tasks

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 /* --- events --- */
 const form = document.querySelector('form');
 const taskInput = document.querySelector('#task');
+const list = document.querySelector('ul');
+const clearBtn = document.querySelector('#clear');
 
-// submit btn
+// events
 form.addEventListener('submit', addTask);
+list.addEventListener('click', delTask);
+clearBtn.addEventListener('click', delAll);
+
 
 // adding task
 function addTask(e) {
 	const li = document.createElement('li');
-	const list = document.querySelector('ul');
 	const link = document.createElement('a');
 	
 	li.className = 'collection-item';
@@ -24,4 +28,22 @@ function addTask(e) {
 
 	taskInput.value = '';
 	e.preventDefault();
+};
+
+// deleting task
+function delTask(e) {
+	if (e.target.textContent == 'X') {
+		if (confirm('Do you want to delete this task?')) {
+			e.target.parentElement.remove();
+		}
+	};
+};
+
+// deleting all
+function delAll() {
+	if (confirm('Do you want to delete all tasks?')) {
+		while (list.firstChild) {
+			list.removeChild(list.firstChild);
+		};
+	}	
 };
